354a04c333fe039ef1d1a3373af8f7aa170f4949
Vishal Ponugoti <vponugot@purdue.edu>
2016-04-20 21:15:21 +0530

    Code task on selected item on tablet

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -74,6 +74,17 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private ForecastAdapter mForecastAdapter;
 
+    /**
+     * A call back interface that all activities containing this fragment must implement.
+     * This mechanism allows activities to be notified of item selections.
+     */
+    public interface Callback {
+        /**
+         * DetailFragmentCallback for when an item has been selected
+         */
+        void onItemSelected(Uri dateUri);
+    }
+
     public ForecastFragment() {
     }
 
