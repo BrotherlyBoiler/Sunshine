ec57dab92eaaa0289c4099cb9210095abf562ea5
Vishal Ponugoti <vponugot@purdue.edu>
2016-04-24 22:57:26 +0530

    Polish app design in main and detail activities

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
