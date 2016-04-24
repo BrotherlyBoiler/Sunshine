79f47a72920ecd27181acc1dadad7cde4487db2e
Vishal Ponugoti <vponugot@purdue.edu>
2016-04-24 16:46:54 +0530

    Make action bar appear in SettingsActivity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
